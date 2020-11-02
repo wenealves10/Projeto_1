90d6c771c6ec252835003e4b2cda9e6590ddee71
Wene Alves <weneplay5@gmail.com>
2020-11-02 14:57:03 -0300

    Criação do model

---
+++ database/Pergunta.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize')
+const connection = require('./database')
+
+const Pergunta = connection.define('perguntas',{
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+},{})
+
+Pergunta.sync({force: false}).then(() => {
+    console.log('Tabela criada com sucesso!')
+}).catch((err) => {
+    console.log('Erro na criação da tabela!: '+err)
+});
