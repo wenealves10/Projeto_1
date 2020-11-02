044e51323c002ef6878e951643863cd5aad69b8f
Wene Alves <weneplay5@gmail.com>
2020-11-02 18:11:59 -0300

    mudando a cor de bg

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const connection = require('./database/database')
 const Pergunta = require('./database/Pergunta')
+const Resposta = require('./database/Resposta')
 const port = 8080
 
 connection
