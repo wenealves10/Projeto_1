8a060e4192072a49cfb994b62143a3983c8faa89
Wene Alves <weneplay5@gmail.com>
2020-11-02 08:48:47 -0300

    testes de forEach no ejs

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+const produtos = [
+    {nome: 'Hamburger', preco: 20},
+    {nome: 'Batata-Fritas', preco: 15},
+    {nome: 'Sanduíche', preco: 25},
+    {nome: 'Pizza de calabresa', preco: 45},
+    {nome: 'Refrigerante coca-cola', preco: 6},
+    {nome: 'Suco de laranja', preco: 10},
+    {nome: 'Coxinha de frango', preco: 12},
+    {nome: 'Pastel de queijo', preco: 14},
+    {nome: 'Macarronada', preco: 27},
+    {nome: 'Açaí', preco: 13},
+]
+
 app.set('view engine','ejs')
 
 app.get('/',(req, res) => {
@@ -13,7 +26,8 @@ app.get('/',(req, res) => {
         nome,
         lang,
         musica: 'Drown lang',
-        mostraHora: (horaCerta.getHours() >= 18 && horaCerta.getHours() <= 23) ? true : false
+        mostraHora: (horaCerta.getHours() >= 18 && horaCerta.getHours() <= 23) ? true : false,
+        produtos,
     })
 })
 
