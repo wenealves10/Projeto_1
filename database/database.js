27f27ce3a6372551d53a1c6b8b91dbf03e3a4acf
Wene Alves <weneplay5@gmail.com>
2020-11-02 19:53:27 -0300

    criando as variaveis ambientes

--- database/database.js
+++ database/database.js
@@ -1,6 +1,7 @@
+require('dotenv/config')
 const Sequelize = require('sequelize')
-const connect = new Sequelize('sistemapr','root','04091605',{
-    host: 'localhost',
+const connect = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 })
 
