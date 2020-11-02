b95309d4ddf8707ac5a63af296e985bf26371a70
Wene Alves <weneplay5@gmail.com>
2020-11-02 19:16:34 -0300

    Fazendo a listagem das respostas

--- index.js
+++ index.js
@@ -9,59 +9,82 @@ const port = 8080
 connection
     .authenticate()
     .then(() => {
-      console.log('Conexão fei com sucesso!')      
+        console.log('Conexão fei com sucesso!')
     }).catch((err) => {
-          console.log('Aconteceu o erro ao se conectar com banco de dados: '+ err)  
+        console.log('Aconteceu o erro ao se conectar com banco de dados: ' + err)
     });
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(bodyParser.json())
 
 
-app.get('/',(req, res) => {
-    Pergunta.findAll({raw: true, order: [
-        ['id','DESC']
-    ]}).then(perguntas =>{
-        res.render('index',{
+app.get('/', (req, res) => {
+    Pergunta.findAll({
+        raw: true,
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(perguntas => {
+        res.render('index', {
             perguntas
         })
     })
 })
 
-app.get('/perguntar', (req, res) =>{
+app.get('/perguntar', (req, res) => {
     res.render('perguntar')
 })
 
-app.post('/salvarpergunta',(req, res) => {
-   let title = req.body.title
-   let description = req.body.description
-   Pergunta.create({
-       title,
-       description
-   }).then(() =>{
-       res.redirect('/')
-   })
-})
-
-app.get('/pergunta/:id', (req, res) =>{
+app.get('/pergunta/:id', (req, res) => {
     let id = req.params.id
     Pergunta.findOne({
         where: {
             id: id
         }
-    }).then(pergunta =>{
-        if(pergunta != undefined){
-            res.render('pergunta',{
-                pergunta
+    }).then(pergunta => {
+        if (pergunta != undefined) {
+            Resposta.findAll({
+                where: {questionID: id}
+            }).then(respostas => {
+                res.render('pergunta', {
+                    pergunta,
+                    respostas
+                })
             })
-        }else{
+        } else {
             res.redirect('/')
-        } 
+        }
     })
 })
 
-app.listen(port, () =>{
+app.post('/salvarpergunta', (req, res) => {
+    let title = req.body.title
+    let description = req.body.description
+    Pergunta.create({
+        title,
+        description
+    }).then(() => {
+        res.redirect('/')
+    })
+})
+
+app.post('/resposta', (req, res) => {
+    let questionID = req.body.questionID
+    let body = req.body.body
+    Resposta.create({
+        questionID,
+        body
+    }).then(() => {
+        res.redirect('/pergunta/' + req.body.questionID)
+    })
+
+})
+
+
+app.listen(port, () => {
     console.log('Servidor rodando.....')
 })
